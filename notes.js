a3f689f631494aa7621991e79aad27baa7e7bb78
Aresky Berkane <areskyberkane@gmail.com>
2020-05-18 12:10:55 +0200

    initial git

---
+++ notes.js
@@ -0,0 +1,13 @@
+// RTC INTRO peerConnection
+
+// get local media
+
+// set up a signalling channel (to negociate peer conection)
+
+// set up a NEW peer connection
+
+// Once it's set up, attach media to connection
+
+// Attach media to connection
+
+// Exange session descriptions
